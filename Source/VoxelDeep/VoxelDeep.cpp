547dfefd32ff5bb5da4a1ac4751572bf7b8936f9
SimonR <sw.roeloffs@hotmail.com>
2022-04-21 16:01:57 +0200

    Initial commit

---
+++ Source/VoxelDeep/VoxelDeep.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VoxelDeep.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VoxelDeep, "VoxelDeep" );
---
+++ Source/VoxelDeep/VoxelDeepGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoxelDeepGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOXELDEEP_API AVoxelDeepGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
